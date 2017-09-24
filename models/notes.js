31f5ba2c361b82512e3b6019f6562c75dcf6018e
Jeannie <jflaherty@utexas.edu>
2017-09-24 12:28:03 -0500

    worked on models

--- models/notes.js
+++ models/notes.js
@@ -1,8 +1,14 @@
 module.exports = function(sequelize, Sequelize) {
   return sequelize.define('student', {
     email: Sequelize.VARCHAR(255),
-    timestamps: false
-    present: Sequelize.BOOLEAN,
-
+    author: Sequelize.VARCHAR(255),
+    note: Sequelize.TEXT,
+    created_at: Sequelize.DATE,
   });
+
 }
+
+
+
+// Syncs with DB
+// Chirp.sync();
