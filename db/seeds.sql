b0bf32f11fde518e8ad1e3376542a7f2c3c5ab4b
manishap1 <manishapawar.net@gmail.com>
2017-09-24 12:54:59 -0500

    seeds.sql modified

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 use sequelize_passportdb;
-insert into users(firstname,lastname,username,about,email,password,status,studentloginType,adminloginType) 
-values("vic","doe","vicdoe@gmail.com","F","vicdoe@gmail.com","abc","active",true,false);
+insert into users(firstname,lastname,username,about,email,password,status,studentloginType,adminloginType,createdAt,updatedAt) 
+values("vic","doe","vicdoe@gmail.com","F","vicdoe@gmail.com","abc","active",true,false,'2016-01-04 02:33:04','2016-01-04 02:33:04');
